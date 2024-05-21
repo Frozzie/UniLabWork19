632acb441ed2494c387b352cf58f38158d546739
Frozzie <yasha.petrukhin@gmail.com>
2024-05-21 19:26:52 +0300

    Correct Task 1 done

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <stdint.h>
 #include <string.h>
+#include "matrix.h"
 
 #define MAX_BUFFER_SIZE    5000001
 
@@ -225,7 +226,6 @@ void brushSort (int *a, size_t n)
 		}
 
 	    step = (size_t)((float)step/factor);
-        //step--;
 	}
 }
 
@@ -291,371 +291,83 @@ void radixSort (int *a, size_t n)
     free(c);
 }
 
-long long getBubbleSortNComp (int *a, size_t n)
+void task1()
 {
-    long long nComps = 0;
-    for (size_t i = 0; ++nComps && i < n; i++)
+    char file[] = {"task1.txt"};
+    char file_res[] = {"task1_a.txt"};
+    FILE *f_orig = fopen(file, "r");
+    if (f_orig == NULL)
     {
-        int min = a[i];
-        for (size_t j = i + 1; ++nComps && j < n; j++)
-        {
-            if (++nComps && min < a[j])
-            {
-                min = a[j];
-            }
-
-            int temp = min;
-            min = a[j];
-            a[j] = temp;
-        }
+        printf ("Task1 error open original file\n");
+        return;
     }
-
-    return nComps;
-}
-
-long long getSelectionSortNComp (int *a, size_t n)
-{
-    long long nComps = 0;
-    for (int i = 1; ++nComps && i < n; i++)
+    FILE *f_res = fopen(file_res, "w");
+    if (f_res == NULL)
     {
-        int newElement = a[i];
-        int location = i - 1;
-
-        while(++nComps && location >= 0 && a[location] > newElement)
-        {
-            a[location + 1] = a[location];
-            location = location - 1;
-        }
-
-        a[location + 1] = newElement;
+        printf ("Task1 error open result file\n");
+        return;
     }
+    int size;
+    //input matrix from file
 
-    return nComps;
-}
+    int input;
+    input = fscanf (f_orig, "%d", &size);
 
-long long getInsertionSortNComp (int *a, size_t n)
-{
-    long long nComps = 0;
-    for (int i = 0; ++nComps && i < n; i++)
+    while(input > 0)
     {
-        int minPosition = i;
-        for (int j = i + 1; ++nComps && j < n; j++)
-        {
-            if (++nComps && a[minPosition] > a[j])
-            {
-                minPosition = j;
-            }
-        }
-
-        int *a_min = &a[minPosition];
-        int *a_cur = &a[i];
-
-        int tmp = *a_min;
-        *a_min = *a_cur;
-        *a_cur = tmp;
-    }
-
-    return nComps;
-}
-
-long long getBrushSortNComp (int *a, size_t n)
-{
-    float factor = 1.05;
-	size_t step = n - 1;
-    long long nComps = 0;
-    
-	while (++nComps && step >= 1)
-	{
-		for (size_t i = 0; ++nComps && i + step < n; i++)
-		{
-            int *a_i = &a[i];
-            int *a_i_step = &a[i + step];
-			if (++nComps && *a_i > *a_i_step)
-			{
-				int temp = *a_i;
-                *a_i = *a_i_step;
-                *a_i_step = temp;
-			}
-		}
-
-	    step = (size_t)((float)step/factor);
-        //step--;
-	}
-
-    return nComps;
-}
-
-long long getShellSortNComp (int *a, size_t n)
-{
-    long long nComps = 0;
-
-    for (int s = n / 2; ++nComps && s > 0; s /= 2) 
-    {
-        for (int i = s; ++nComps && i < n; ++i) 
-        {
-            for (int j = i - s; ++nComps && j >= 0 && a[j] > a[j + s]; j -= s) 
-            {
-                int temp = a[j];
-                a[j] = a[j + s];
-                a[j + s] = temp;
-            }
-        }
-    }
-
-    return nComps;
-}
-
-long long getRadixSortNComp (int *a, size_t n)
-{
-    const size_t len = sizeof(int);     // длина числа
-    const size_t radix = 256;           // количество чисел в разряде 2^8 // основание системы счисления
-    long long nComps = 0;
-    size_t basket_len = n;
-    uint8_t d;
-
-    if (basket_len > 1500000)
-    {
-        basket_len = 1500000;
-    }
-
-    int *b = malloc(sizeof(int) * radix),       // число чисел в корзине
-        *c = malloc(sizeof(int) * radix * basket_len);   // корзины для сортировки radix
-
-    for (size_t i = 0; ++nComps && i < len; i++)  // перебираем все разряды, начиная с нулевого
-    {   
-        memset (b, 0, sizeof(int) * radix); // пустой массив корзин b = 0  
-
-        for (size_t j = 0; ++nComps && j < n; j++)
-        {
-            int val = a[i];
-            d = digit(val, i);              // получаем цифру, стоящую на текущем разряде в каждом числе массива
-            int index = (d * basket_len) + b[d];     // индекс записи
-            c[index] = val;                 // отправляем число в промежуточный массив в корзину, которая совпадает со значением этой цифры
-            b[d]++;                         // добавляем число чисел в корзине
-        }
-
-        size_t count = 0;
+        int *arr = malloc(sizeof(int) * size * size);
         
-        // сложим обратно числа из корзин в массив a
-        for (size_t j = 0; ++nComps && j < radix; j++)
+        for(int i = 0; i < size * size; i++)
         {
-            int k = 0;
-            int bj = b[j];
-            while (++nComps && k < bj)
+            fscanf(f_orig, "%d", &arr[i]);
+        }
+
+        matrix mat = createMatrixFromArray(arr, size, size);
+        
+        //transposeMatrix(&mat);
+
+        //output matrix to new file
+
+        fprintf(f_res,"%d\n", size);
+
+        for(int col = 0; col < size; col++)
+        {
+            for(int row = 0; row < size; row++)
             {
-                a[count++] = c[(j * basket_len) + k];
-                k++;
+                fprintf(f_res,"%d ", getElementMatrix(&mat, row, col));
             }
+            fprintf(f_res,"\n");
         }
+        
+        input = fscanf (f_orig, "%d", &size);
     }
-    free(b);
-    free(c);
 
-    return nComps;
+    fclose(f_orig);
+    fclose(f_res);
+
+    remove(file);
+    rename(file_res, file);
 }
 
-void checkTime (void(*sortFunc)(int *, size_t), 
-                void (*generateFunc)(int *, size_t), 
-                size_t size, 
-                char *experimentName) 
+void task2()
 {
-    static size_t runCounter = 1;
+    // char *file = malloc(strlen()); // first argument is file name in
+    // strcpy(file, );
 
-    // генерация последовательности
-    static int innerBuffer[MAX_BUFFER_SIZE];
-    generateFunc(innerBuffer, size);
+    // int size = atoi(); // second argument is max num
 
-    printf("Run #%zu| ", runCounter++);
-    printf("Name: %s\n", experimentName);
-    double time;
-    TIME_TEST(
-    {
-        sortFunc(innerBuffer, size);
-    }, time);
+    // FILE *f = fopen(file, "r");
 
-    printf("Status: ");
-    if (isOrdered(innerBuffer, size)) 
-    {
-        printf("OK! Time: %.3f s.\n", time);
+    // int *arr = malloc(sizeof(int) * size * size); 
 
-        // запись в файл
-        char filename[256];
-        // sprintf(filename, "./data/%s.csv", experimentName);
-        sprintf(filename, "%s.csv", experimentName);
-
-        FILE *f = fopen(filename, "a");
-        if (f == NULL) 
-        {
-            printf("FileOpenError %s", filename);
-            exit(1);
-        }
-
-        fprintf(f, "%zu, %.3f\n", size, time);
-        fclose(f);
-    } 
-    else 
-    {
-        printf("Wrong!\n");
-        // вывод массива, который не смог быть отсортирован
-        outputArrayHex(innerBuffer, size);
-
-        exit(1);
-    }
-}
-
-void checkTimeNComp (long long(*sortFunc)(int *, size_t), 
-                    void (*generateFunc)(int *, size_t), 
-                    size_t size, 
-                    char *experimentName)
-{
-    static size_t runCounter = 1;
-
-    // генерация последовательности
-    static int innerBuffer[MAX_BUFFER_SIZE];
-    generateFunc(innerBuffer, size);
-
-    printf("Run #%zu| ", runCounter++);
-    printf("Name: %s\n", experimentName);
-    double time;
-    long long nComp;
-    TIME_TEST(
-    {
-        nComp = sortFunc(innerBuffer, size);
-    }, time);
-
-    printf("Status: ");
-    if (isOrdered(innerBuffer, size)) 
-    {
-        printf("OK! Time: %.3f s.\n", time);
-        printf("Comps: %lld \n", nComp);
-
-        // запись в файл
-        char filename[256];
-        // sprintf(filename, "./data/%s.csv", experimentName);
-        sprintf(filename, "%s.csv", experimentName);
-
-        FILE *f = fopen(filename, "a");
-        if (f == NULL) 
-        {
-            printf("FileOpenError %s", filename);
-            exit(1);
-        }
-
-        fprintf(f, "%zu, %.3f, %lld\n", size, time, nComp);
-        fclose(f);
-    } 
-    else 
-    {
-        printf("Wrong!\n");
-        // вывод массива, который не смог быть отсортирован
-        outputArrayHex(innerBuffer, size);
-
-        exit(1);
-    }
-}
-
-void timeExperiment() 
-{
-    // описание функций сортировки
-    SortFunc sorts[] = 
-    {
-        {bubbleSort, "bubbleSort"},
-        {selectionSort, "selectionSort"},
-        {insertionSort, "insertionSort"},
-        {brushSort, "brushSort"},
-        {shellSort, "shellSort"},
-        {radixSort, "radixSort"},
-        {mySort, "mySort"}
-        // вы добавите свои сортировки
-    };
-    const unsigned FUNCS_N = ARRAY_SIZE(sorts);
-
-    // описание функций генерации
-    GeneratingFunc generatingFuncs[] = 
-    {
-        // генерируется случайный массив
-        {generateRandomArray, "random"},
-        // генерируется массив 0, 1, 2, ..., n - 1
-        {generateOrderedArray, "ordered"},
-        // генерируется массив n - 1, n - 2, ..., 0
-        {generateOrderedBackwards, "orderedBackwards"}
-    };
-    const unsigned CASES_N = ARRAY_SIZE(generatingFuncs);
-
-    // запись статистики в файл up to MAX_BUFFER_SIZE
-    for (size_t size = 1000; size <= 10000; size += 1000)
-    {
-        printf("------------------------------\n");
-        printf("Size: %d\n", size);
+    // matrix mat = createMatrixFromArray(arr, size, size);
     
-        for (int i = 0; i < FUNCS_N; i++) 
-        {
-            for (int j = 0; j < CASES_N; j++) 
-            {
-                // генерация имени файла
-                static char filename[128];
-            
-                sprintf (filename, "%s_%s_time", sorts[i].name, generatingFuncs[j].name);
-            
-                checkTime(sorts[i].sort, generatingFuncs[j].generate, size, filename);
-            }
-        }
-        printf("\n");
-    }
 }
-
-void timeExperimentNComp() 
+int main(int argc, char **argv) 
 {
-    // описание функций сортировки
-    SortFuncNComp sorts[] = 
-    {
-        {getBubbleSortNComp, "getBubbleSortNComp"},
-        {getSelectionSortNComp, "getSelectionSortNComp"},
-        {getInsertionSortNComp, "getInsertionSortNComp"},
-        {getBrushSortNComp, "getBrushSortNComp"},
-        {getShellSortNComp, "getShellSortNComp"},
-        {getRadixSortNComp, "getRadixSortNComp"}
-        // вы добавите свои сортировки
-    };
-    const unsigned FUNCS_N = ARRAY_SIZE(sorts);
-
-    // описание функций генерации
-    GeneratingFunc generatingFuncs[] = 
-    {
-        // генерируется случайный массив
-        {generateRandomArray, "random"},
-        // генерируется массив 0, 1, 2, ..., n - 1
-        {generateOrderedArray, "ordered"},
-        // генерируется массив n - 1, n - 2, ..., 0
-        {generateOrderedBackwards, "orderedBackwards"}
-    };
-    const unsigned CASES_N = ARRAY_SIZE(generatingFuncs);
-
-    // запись статистики в файл up to MAX_BUFFER_SIZE
-    for (size_t size = 500; size <= 5000; size += 500)
-    {
-        printf("------------------------------\n");
-        printf("Size: %d\n", size);
+    task1();
     
-        for (int i = 0; i < FUNCS_N; i++) 
-        {
-            for (int j = 0; j < CASES_N; j++) 
-            {
-                // генерация имени файла
-                static char filename[128];
-            
-                sprintf (filename, "%s_%s_time", sorts[i].name, generatingFuncs[j].name);
-            
-                checkTimeNComp(sorts[i].sort, generatingFuncs[j].generate, size, filename);
-            }
-        }
-        printf("\n");
-    }
-}
-
-int main() 
-{
-    timeExperiment();
+    //timeExperiment();
     //timeExperimentNComp();
 
     return 0;
